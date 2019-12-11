f8f0c455a951fc335bfce50a51819e23280e5f2c
Atelier Mirai <mirai@atelier-mirai.net>
2019-12-11 17:38:45 +0900

    余白等調整中

--- generator.rb
+++ generator.rb
@@ -1,13 +1,5 @@
-# include template
-require './ikadzuchi.rb'
+# 施工事例生成モジュール
+require 'ikadzuchi.rb'
 
-# 工事日 R01-11-26など
-construction_date = ARGV[0]
-
-if construction_date.nil?
-  # 全ての工事の施行事例を生成
-  Ikadzuchi::generate_all
-else
-  # 個別の工事の施行事例を生成
-  Ikadzuchi::generate(construction_date)
-end
+# 全ての工事の施行事例を生成
+Ikadzuchi::generate_all
