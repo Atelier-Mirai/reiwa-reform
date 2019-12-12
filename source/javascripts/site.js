2292f3613056871da764a19e8d92bd6b704c57ad
Atelier Mirai <mirai@atelier-mirai.net>
2019-12-12 12:51:05 +0900

    css/js build完了

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,32 +1,33 @@
-$(() => {
-  page_top
+$(function(){
+  // page_top
   $("#page_top").hide();
-  $(window).scroll(() => {
+  $(window).scroll(function(){
     if ($(this).scrollTop() > 100) { //100pxスクロールしたら表示
       $("#page_top").fadeIn();
     } else {
       $("#page_top").fadeOut();
     }
   });
-  $("#page_top").on("click", () => {
+  $("#page_top").on("click", function() {
     $("body, html").animate({
       scrollTop: 0
     }, 500); //0.5秒かけてトップへ移動
     return false;
   });
 
+  // pagination
   // 最初は active 以外のコンテンツを非表示に
   $(".page-content").hide();
   $(".page-content.active").show();
-  let active_id = $(".pagination .page-item.active a").attr("href");
-  $(`${active_id}`).show();
+  var active_id = $(".pagination .page-item.active a").attr("href");
+  $("$("+active_id+")").show();
   // page がクリックされたときに、そのコンテンツを表示
   $(".page-item").on("click", function() {
     $(".page-content").hide();
-    let time = 100;
-    let href = $(this).find("a").attr("href");
-    let target = $(href);
-    let distance = target.offset().top;
+    var time = 100;
+    var href = $(this).find("a").attr("href");
+    var target = $(href);
+    var distance = target.offset().top;
     $($(this).find("a").attr("href")).show();
     $("html, body").animate({
       scrollTop: distance
@@ -35,7 +36,7 @@ $(() => {
   });
 
   // Porfolio isotope and filter
-  const portfolioIsotope = $(".portfolio-container").isotope({
+  var portfolioIsotope = $(".portfolio-container").isotope({
     itemSelector: ".portfolio-item",
     layoutMode: "fitRows"
   });
