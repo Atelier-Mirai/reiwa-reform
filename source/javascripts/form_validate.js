3f8deb0cf35a7bb43b61356bb4c2bcf39de6f915
Atelier-Mirai <contact@atelier-mirai.net>
2020-05-19 13:12:37 +0900

    お問い合わせフォーム 完成

--- source/javascripts/form_validate.js
+++ source/javascripts/form_validate.js
@@ -14,3 +14,23 @@ validate();
 document.querySelectorAll("input,textarea").forEach(input => {
   input.addEventListener("input", validate);
 });
+
+// 参照元URLをセット
+document.getElementById("referrer_url").value = document.referrer;
+
+// フォーム送信前にタブを閉じる際に、確認アラートを表示する。
+const confirmation_aleat = function (e) {
+    // カスタムメッセージの設定（IE/Edgeのみ有効)
+    const confirmMessage = '入力欄を破棄し、離脱します。よろしいですか？';
+    e.returnValue = confirmMessage;
+    return confirmMessage;
+};
+
+// beforeunloadイベントの登録
+window.addEventListener('beforeunload', confirmation_aleat, false);
+
+// submit ボタンが押されたときには、離脱アラートを表示しない。
+document.getElementById('submit').addEventListener('click', () => {
+    // submit時はアラート表示させない
+    window.removeEventListener('beforeunload', confirmation_aleat, false);
+});
